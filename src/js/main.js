0d7e900937116fd3994e2218a2a033f6b7952596
Ivan Gregor Tabalno <igmtofficial@gmail.com>
2022-06-13 22:49:10 +0800

    fixed products filter button mobile.

--- src/js/main.js
+++ src/js/main.js
@@ -74,15 +74,18 @@ const filterList = document.querySelector(".filter-list");
 const filterItem = document.querySelectorAll(".filter-item");
 const filterItemTitle = document.querySelectorAll(".filter-item-title");
 
-filterBtnMbl.addEventListener("click", function () {
+const toggleNav = function () {
   filterArrowIcon.classList.toggle("bx-chevron-up");
   //   filterList.classList.toggle("hidden");
   filterList.classList.toggle("pointer-events-none");
   filterList.classList.toggle("opacity-0");
-});
+};
+
+filterBtnMbl.addEventListener("click", toggleNav);
 
 for (let i = 0; i < filterItem.length; i++) {
   filterItem[i].addEventListener("click", function () {
     filterBtnTitle.textContent = filterItemTitle[i].textContent;
+    toggleNav();
   });
 }
