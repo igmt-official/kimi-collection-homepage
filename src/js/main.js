d25dabf9624523b5f4f19c9da3be3ba66fa2baee
Ivan Gregor Tabalno <igmtofficial@gmail.com>
2022-06-13 11:22:45 +0800

    updated pagination navigation mobile.

--- src/js/main.js
+++ src/js/main.js
@@ -31,10 +31,10 @@ const activePage = function (position) {
   // To remove/add classes in all elements
   for (let i = 0; i < navLink.length; i++) {
     navLink[i].classList.remove("py-3");
-    navLink[i].classList.remove("px-4");
+    navLink[i].classList.remove("px-5");
     navLink[i].classList.remove("bg-zinc-800");
     navLink[i].classList.remove("flex");
-    navLink[i].classList.remove("justify-between");
+    navLink[i].classList.remove("justify-center");
     navLink[i].classList.remove("items-center");
     navLink[i].classList.remove("gap-2");
     navLink[i].classList.remove("rounded-full");
@@ -47,10 +47,10 @@ const activePage = function (position) {
   //  (This) keyword to add the class name on (navLink addEventListener) position
   //  To remove/add classes in element specific position
   navLink[position].classList.add("py-3");
-  navLink[position].classList.add("px-4");
+  navLink[position].classList.add("px-5");
   navLink[position].classList.add("bg-zinc-800");
   navLink[position].classList.add("flex");
-  navLink[position].classList.add("justify-between");
+  navLink[position].classList.add("justify-center");
   navLink[position].classList.add("items-center");
   navLink[position].classList.add("gap-2");
   navLink[position].classList.add("rounded-full");
