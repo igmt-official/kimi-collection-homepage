d3ba1e1f79b79fbe55ce71bb597e83826c7de6bc
Ivan Gregor Tabalno <igmtofficial@gmail.com>
2022-06-13 11:16:41 +0800

    added pagination navigation mobile.

--- src/js/main.js
+++ src/js/main.js
@@ -22,4 +22,46 @@
 //   }
 // });
 
-// !Pagination Navbar
+// !Pagination Navigation Mobile
+const navLink = document.querySelectorAll(".nav-link");
+const navText = document.querySelectorAll(".nav-text");
+const navPage = document.querySelectorAll(".nav-page");
+
+const activePage = function (position) {
+  // To remove/add classes in all elements
+  for (let i = 0; i < navLink.length; i++) {
+    navLink[i].classList.remove("py-3");
+    navLink[i].classList.remove("px-4");
+    navLink[i].classList.remove("bg-zinc-800");
+    navLink[i].classList.remove("flex");
+    navLink[i].classList.remove("justify-between");
+    navLink[i].classList.remove("items-center");
+    navLink[i].classList.remove("gap-2");
+    navLink[i].classList.remove("rounded-full");
+
+    navText[i].classList.add("hidden");
+
+    navPage[i].classList.add("hidden");
+  }
+
+  //  (This) keyword to add the class name on (navLink addEventListener) position
+  //  To remove/add classes in element specific position
+  navLink[position].classList.add("py-3");
+  navLink[position].classList.add("px-4");
+  navLink[position].classList.add("bg-zinc-800");
+  navLink[position].classList.add("flex");
+  navLink[position].classList.add("justify-between");
+  navLink[position].classList.add("items-center");
+  navLink[position].classList.add("gap-2");
+  navLink[position].classList.add("rounded-full");
+
+  navText[position].classList.remove("hidden");
+
+  navPage[position].classList.remove("hidden");
+};
+
+for (let i = 0; i < navLink.length; i++) {
+  navLink[i].addEventListener("click", function () {
+    activePage(i);
+  });
+}
